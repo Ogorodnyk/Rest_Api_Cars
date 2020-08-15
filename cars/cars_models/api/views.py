e2bb124c0b8c8defad7f806ced5b611f5f9cbaf1
Maksym Ogorodnyk <maksym.ogorodnyk1@gmail.com>
2020-08-15 13:11:54 +0200

    First_Post_View

--- cars/cars_models/api/views.py
+++ cars/cars_models/api/views.py
@@ -6,12 +6,20 @@ from cars_models.models import Car
 from cars_models.api.serializers import CarSerializer
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def car_list_create_api_view(request):
+
     if request.method == "GET":
         cars = Car.objects.all()
         serializer = CarSerializer(cars, many=True)
         return Response(serializer.data)
+
+    elif request.method == "POST":
+        serializer = CarSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET"])

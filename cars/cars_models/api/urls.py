3bd2141d45ff59b7d4ed571be5373147971ac4ab
Maksym Ogorodnyk <maksym.ogorodnyk1@gmail.com>
2020-08-16 15:13:15 +0200

    CreateView

--- cars/cars_models/api/urls.py
+++ cars/cars_models/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from cars_models.api.views import car_list_create_api_view, car_list_popular_api_view
+from cars_models.api.views import CarListCreateAPIView, CarDetailAPIView
+from cars_models.api.views import car_list_popular_api_view
+# from cars_models.api.views import car_list_create_api_view, car_list_popular_api_view, car_detail_api_view
 
 urlpatterns = [
-    path("cars/", car_list_create_api_view, name="cars-list"),
     path("popular/", car_list_popular_api_view, name="popular-cars-list"),
+    path("cars/<int:pk>", CarDetailAPIView.as_view(), name="cars-detail"),
+    path("cars/", CarListCreateAPIView.as_view(), name="cars-list"),
+
 ]
--- cars/cars_models/api/views.py
+++ cars/cars_models/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
 
 from cars_models.models import Car
 from cars_models.api.serializers import CarSerializer
@@ -8,7 +10,6 @@ from cars_models.api.serializers import CarSerializer
 
 @api_view(["GET", "POST"])
 def car_list_create_api_view(request):
-
     if request.method == "GET":
         cars = Car.objects.all()
         serializer = CarSerializer(cars, many=True)
@@ -22,9 +23,81 @@ def car_list_create_api_view(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(["GET", "PUT", "DELETE"])
+def car_detail_api_view(request, pk):
+    try:
+        car = Car.objects.get(pk=pk)
+    except Car.DoesNotExist:
+        return Response({"error": {
+            "code": 404,
+            "message": "Car not found!"
+        }}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = CarSerializer(car)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = CarSerializer(car, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        car.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 @api_view(["GET"])
 def car_list_popular_api_view(request):
     if request.method == "GET":
         cars = Car.objects.order_by("-rate")
         serializer = CarSerializer(cars, many=True)
         return Response(serializer.data)
+
+
+
+
+class CarListCreateAPIView(APIView):
+
+    def get(self, request):
+        cars = Car.objects.all()
+        # rates = CarRates.objects.all()
+        cars_serializer = CarSerializer(cars, many=True)
+        # rates_serializer = CarRatesSerializer(rates, many=True)
+        response = cars_serializer.data
+        return Response(response)
+
+    def post(self, request):
+        serializer = CarSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CarDetailAPIView(APIView):
+
+    def get_object(self, pk):
+        car = get_object_or_404(Car, pk=pk)
+        return car
+
+    def get(self, request, pk):
+        car = self.get_object(pk)
+        serializer = CarSerializer(car)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        car = self.get_object(pk)
+        serializer = CarSerializer(car, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        car = self.get_object(pk)
+        car.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+

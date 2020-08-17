fb96d9ef75163aab1426b6fb6b278f4de66ff790
Maksym Ogorodnyk <maksym.ogorodnyk1@gmail.com>
2020-08-17 18:15:55 +0200

    test

---
+++ README.md
@@ -0,0 +1,37 @@
+ REST API CARS
+
+
+Application URL 
+
+Prerequisites
+To start you have to have installed Python 3.8
+
+Getting Started
+To start the project locally
+
+Clone repository
+
+$ git clone https://github.com/Ogorodnyk/Rest_Api_Cars.git
+I recommend using virtualenv
+
+$ virtualenv venv
+
+for unix users:
+$ source venv/bin/activate 
+
+for windows users:
+$ source venv/Scripts/activate
+then
+
+next step is to install all required packages
+
+$ pip install -r requirements.txt
+before running app, make all needed migrations
+
+$ python manage.py migrate
+now you can start
+
+$ python manage.py runserver
+open a browser and go to
+
+http://127.0.0.1:8000
--- cars/cars_models/tests.py
+++ cars/cars_models/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
 
 # Create your tests here.
+import pytest
+
+from .models import Car, GiveRate
+
+
+@pytest.mark.django_db
+def test_post_car(client, set_up):
+    car_before = Car.objects.count()
+    new_car = {
+        "car_make": "Bmw",
+        "model_name": "x6",
+    }
+    response = client.post("/movies/", new_car, format='json')
+    assert response.status_code == 201
+    assert Car.objects.count() == car_before + 1
+    for key, value in new_car.items():
+        assert key in response.data
+        if isinstance(value, list):
+            # Compare contents regardless of their order
+            assert len(response.data[key]) == len(value)
+        else:
+            assert response.data[key] == value
+
+
+@pytest.mark.django_db
+def test_get_car_list(client, set_up):
+    response = client.get("/cars/", {}, format='json')
+
+    assert response.status_code == 200
+    assert Car.objects.count() == len(response.data)
+
+
+@pytest.mark.django_db
+def test_get_car_detail(client, set_up):
+    response = client.get("/cars/1/", {}, format='json')
+
+    assert response.status_code == 200
+    for field in ('id', 'car_make', 'model_name', 'average_rates'):
+        assert field in response.data
+
+
+@pytest.mark.django_db
+def test_get_car_popular(client, set_up):
+    response = client.get("/popular/", {}, format='json')
+
+    assert response.status_code == 200
+    for field in ('id', 'car_make', 'model_name', 'rate'):
+        assert field in response.data
